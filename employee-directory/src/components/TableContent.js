acd2191ff5943dcfd7a65bf6ac87c9205f204a2c
Jim Faulkner <jhf1203@gmail.com>
2020-11-04 21:33:14 -0500

    styling

--- employee-directory/src/components/TableContent.js
+++ employee-directory/src/components/TableContent.js
@@ -1,32 +1,16 @@
-import React, { useState } from "react";
-import employees from "../employees.json"
-import tableWrapper from "./TableWrapper"
-
-// How to render this within the table element in TableWrapper?
-// - Use divs for each row in TableWrapper?
-// - Let <table> live in App.js, have one component for the header and one for the content?
-// - Render this as a second table that will have the same formatting as TableWrapper?
+import React from "react";
 
 function TableContent(props) {
-
-    const [contentState, setContentState] = useState({
-        employees
-      });
-      //console.log(contentState)
-
-
-      return (
-    
-        <tr className="employeeRow" key={props.id}>
-            <td className="colNameF">{props.employee.firstName}</td>
-            <td className="colNameL">{props.employee.lastName}</td>
-            <td className="colEmail">{props.employee.email}</td>
-            <td className="colDept">{props.employee.department}</td>
-            <td className="colHireDate">{props.employee.date}</td>
-            <td className="colGender">{props.employee.gender}</td>
-         </tr>
-);
-  
+  return (
+    <tr className="employeeRow" key={props.id}>
+      <td className="colNameF tdContent">{props.employee.firstName}</td>
+      <td className="colNameL tdContent">{props.employee.lastName}</td>
+      <td className="colEmail tdContent">{props.employee.email}</td>
+      <td className="colDept tdContent">{props.employee.department}</td>
+      <td className="colHireDate tdContent">{props.employee.date}</td>
+      <td className="colGender tdContent">{props.employee.gender}</td>
+    </tr>
+  );  
 }
 
 export default TableContent;
--- employee-directory/src/components/TableWrapper.js
+++ employee-directory/src/components/TableWrapper.js
@@ -4,19 +4,14 @@ import employees from "../employees.json"
 
 function TableWrapper(props) {
 
-    
   const [wrapperState, setWrapperState] = useState({
     employees,
     filteredEmps: [],
   });
 
-    
-
-// Will need functions here to filter and sort
-
 function handleSortAsc(field) {
   
-    var newFilteredAsc = employees.sort(( a, b ) => {
+    let newFilteredAsc = employees.sort(( a, b ) => {
       if ( a[field] < b[field] ){
         return -1;
       }
@@ -26,14 +21,12 @@ function handleSortAsc(field) {
       return 0;
     })
   
-    
-    console.log('field', field)
     setWrapperState({...wrapperState, filteredEmps: newFilteredAsc, })
   
   }
   
 function handleSortDesc(field) {
-  var newFilteredAsc = employees.sort(( a, b ) => {
+  let newFilteredDesc = employees.sort(( a, b ) => {
     if ( a[field] > b[field] ){
       return -1;
     }
@@ -44,33 +37,23 @@ function handleSortDesc(field) {
   })
 
   
-  console.log('field', field)
-  setWrapperState({...wrapperState, filteredEmps: newFilteredAsc, })
+  setWrapperState({...wrapperState, filteredEmps: newFilteredDesc, })
 }
 
-  var handleFilter = (event) => {
-    console.log('HANDLE FILTER!!!!', event.target.value)
+  const handleFilter = (event) => {
 
-    var newFiltered = []
+    const newFiltered = []
 
     wrapperState.employees.map (employee => {
-      if (employee[event.target.name].substring(0, event.target.value.length) === event.target.value) {
-        console.log(' found a match!!')
+      if (employee[event.target.name].toLowerCase().substring(0, event.target.value.length) === event.target.value.toLowerCase()) {
         newFiltered.push(employee)
       }
     })
 
-    console.log('new filitered ppeeeps!!!', newFiltered)
     setWrapperState({...wrapperState, filteredEmps: newFiltered })
-  // setWrapperState({
-  //   ...employees,
-  //   employees.event.target.name,
-  //   ...employees
-  // })
-}
-console.log('this is wrapper state', wrapperState)
 
-  var empsToDisplay = wrapperState.employees
+}
+  let empsToDisplay = wrapperState.employees
 
   if (wrapperState.filteredEmps.length > 0) {
     empsToDisplay = wrapperState.filteredEmps
@@ -80,34 +63,34 @@ console.log('this is wrapper state', wrapperState)
 
   return (
     <div>
-      <header>
-        <h1>Some Header Text</h1>
+      <header className="jumbotron">
+        <h1>Our Team</h1>
       </header>
-     <div className="container">
+     <div className="container-fluid">
        <table>
         <tr className="headRow">
-          <th className="nameF">First Name</th>
-          <th className="nameL">Last Name</th>
-          <th className="eMail">E-Mail</th>
-          <th className="dept">Department</th>
-          <th className="hireDate">Hire Date</th>
-          <th className="gender">Gender</th>
+          <th className="nameF colHead">First Name</th>
+          <th className="nameL colHead">Last Name</th>
+          <th className="eMail colHead">E-Mail</th>
+          <th className="dept colHead">Department</th>
+          <th className="hireDate colHead">Hire Date</th>
+          <th className="gender colHead">Gender</th>
         </tr>
         <tr className="inputRow">
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="firstName" onChange={handleFilter}></input></td>
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="lastName" onChange={handleFilter}></input></td>
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="email" onChange={handleFilter}></input></td>
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="department" onChange={handleFilter}></input></td>
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="date" onChange={handleFilter}></input></td>
-          <td><input className="filterInput" type="text" placeholder="Filter Here" name="gender" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="firstName" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="lastName" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="email" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="department" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="date" onChange={handleFilter}></input></td>
+          <td className="colHead"><input className="filterInput" type="text" placeholder="Filter Here" name="gender" onChange={handleFilter}></input></td>
         </tr>
         <tr className="sortRow">
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('firstName')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('firstName')}></i></p></td>
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('lastName')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('lastName')}></i></p></td>
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('email')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('email')}></i></p></td>
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('department')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('department')}></i></p></td>
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('date')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('date')}></i></p></td>
-          <td className="sortField"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('gender')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('gender')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('firstName')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('firstName')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('lastName')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('lastName')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('email')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('email')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('department')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('department')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('date')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('date')}></i></p></td>
+          <td className="sortField colHead"><p className="sortText">Sort<i className="button btn fa fa-caret-up" onClick={() => handleSortAsc('gender')}></i><i className="button btn fa fa-caret-down" onClick={() => handleSortDesc('gender')}></i></p></td>
         </tr>
         {empsToDisplay.map((employee)=> { 
            return ( <TableContent employee={employee} />)
